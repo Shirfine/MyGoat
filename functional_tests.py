dbcdfcdfc8e16b31aff7ac58c62057c9634ab1a1
Shirfine <469447282@qq.com>
2016-11-17 16:18:45 +0800

    finih one list test, and write the new functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -35,31 +35,27 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         table = self.browser.find_element_by_id('id_list_table')
 
-        rows = table.find_elements_by_name('tr')
-        self.assertTrue(
-            any(row.text =='1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了 “ Use peacock feathers to make a fly ” （使用孔雀羽毛做假蝇）
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # 伊迪丝想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
         # 而且页面中有一些文字解说这个功能
         # 她访问那个URL，发现她的待办事项列表还在
 
+        self.fail('Finish the test!')
 if __name__ == '__main__':
     # 启动测试程序 与 禁止抛出ResourceWaring异常
     unittest.main(warnings='ignore')
-
-
-
-
-
-
-
-
--- lists/tests.py
+++ lists/tests.py
@@ -22,7 +22,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html', {'new_item_text': ''}, request=request)
         # self.assertEqual(response.content.decode(), expected_html)
 
     def test_home_page_can_save_a_POST_request(self):
@@ -32,6 +32,3 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
         self.assertIn('A new list item', response.content.decode())
-
-
-
