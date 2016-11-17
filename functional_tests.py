0489099b5932ae8f1739d21baf01bd2023a197f9
Shirfine <469447282@qq.com>
2016-11-17 16:50:47 +0800

    finish a Restructure

--- functional_tests.py
+++ functional_tests.py
@@ -8,6 +8,11 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def tearDown(self):
         self.browser.quit()
 
@@ -33,10 +38,7 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车键后, 页面更新啦
         # 待办事项表格中显示了'1: Buy peacock feathers'
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了 “ Use peacock feathers to make a fly ” （使用孔雀羽毛做假蝇）
@@ -45,10 +47,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # 伊迪丝想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
@@ -56,6 +56,8 @@ class NewVisitorTest(unittest.TestCase):
         # 她访问那个URL，发现她的待办事项列表还在
 
         self.fail('Finish the test!')
+
+
 if __name__ == '__main__':
     # 启动测试程序 与 禁止抛出ResourceWaring异常
     unittest.main(warnings='ignore')
