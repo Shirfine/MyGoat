1adfcbebe8a3ac9d540904e19d0b146f74fad9eb
Shirfine <469447282@qq.com>
2016-11-17 17:33:13 +0800

    Write the first models and find finish test.py first question

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 
 # Create your tests here.
@@ -11,13 +12,6 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-
-    # def test_home_page_returns_correct_html(self):
-    #     request = HttpRequest()
-    #     response = home_page(request)
-    #     self.assertTrue(response.content.startswith(b'<html>'))
-    #     self.assertIn(b'<title>To-Do lists</title>', response.content)
-    #     self.assertTrue(response.content.strip().endswith(b'</html>'))
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
@@ -32,3 +26,43 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
         self.assertIn('A new list item', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
