f25466f1bd2838ca7c5ad43998027a8409b837ba
Shirfine <469447282@qq.com>
2016-11-28 12:21:34 +0800

    clear models five bugs

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
@@ -18,5 +18,8 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
+
+
