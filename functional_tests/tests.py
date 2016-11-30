f5c57e4b913a93efac2af643e4a5cd5c5a7d8825
Shirfine <469447282@qq.com>
2016-11-30 09:31:29 +0800

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -88,3 +88,17 @@ class NewVisitorTest(LiveServerTestCase):
         self.fail('Finish the test!')
 
         # 她访问那个URL，发现她的待办事项列表还在
+
+    def test_layout_and_styling(self):
+        # 伊迪斯访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 她看到输入框完美地居中显示
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
+
+        # # 她新建了一个清单, 看到输入框仍完美地居中显示
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
