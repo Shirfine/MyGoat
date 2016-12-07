fab91f65ec490e72e62d6f97a6bb9af46bcb3315
Shirfine <469447282@qq.com>
2016-12-07 10:12:41 +0800

    change fabfile.py

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -10,7 +10,7 @@ def deploy():
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
-    _update_settings(source_folder)
+    _update_settings(source_folder, env.host)
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
