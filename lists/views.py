412d394a1439f909223a7d4f9386d56fd78bf14b
Shirfine <469447282@qq.com>
2016-12-09 10:07:59 +0800

    Move hard-coded urls out of templates

--- lists/views.py
+++ lists/views.py
@@ -31,7 +31,7 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
         item.save()
