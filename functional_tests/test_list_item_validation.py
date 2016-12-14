753e0f04fb26318747abefff928184f8eb839414
Shirfine <469447282@qq.com>
2016-12-14 15:44:30 +0800

    Impement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,5 +1,5 @@
 from .base import FunctionalTest
-
+from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
@@ -29,4 +29,23 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
+
+    def test_cannot_add_duplicate_items(self):
+        # 伊迪斯访问首页，新建一个清单
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy wellies')
+
+        # 她不小心输入了一个重复待办事项
+        self.get_item_input_box().send_keys('Buy wellies\n')
+
+        # 她看到一条有帮助的错误消息
+        self.check_for_row_in_list_table('1: Buy wellies')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You've already got this in your list")
+
+
+
+
 
--- lists/models.py
+++ lists/models.py
@@ -11,3 +11,7 @@ class List(models.Model):
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
+
+    class Meta:
+        unique_together = ('list', 'text')
+
