3fc39f83e7afe301f1e1fa0454660240a76b0b90
Shirfine <469447282@qq.com>
2016-12-15 17:30:48 +0800

    add qunit.css.js

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,6 +1,7 @@
 from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
 
+
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # 伊迪斯访问首页,提交了一个空待办事项
@@ -10,7 +11,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 首页刷新了, 显示了一个错误消息
         # 提示待办事项不能为空
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # 她输入了一些文字, 再次提交就没问题了
@@ -22,7 +23,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 在清单页面她看到了一个类似待错误消息
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # 输入文字后就没问题了
@@ -42,10 +43,25 @@ class ItemValidationTest(FunctionalTest):
 
         # 她看到一条有帮助的错误消息
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
 
+    def test_error_messages_are_cleared_on_input(self):
+        # 伊迪斯新建一个清单， 但方法不当， 所以出现了一个验证错误
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
 
+        # 为了消除错误， 她开始在输入框中输入内容
+        self.get_item_input_box().send_keys('a')
+
+        # 看到错误消息消失了，她很高兴
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
 
 
 
